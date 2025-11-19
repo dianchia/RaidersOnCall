7f464b0919c652f2489640b40ca46155b5f0dad2
dianchia <dianrui520@hotmail.com>
2025-11-19 22:14:13 +0800

    Update to game version 1.0.2

--- src/main/java/RaidersOnCall/items/consumables/SummonRaidScrollItem.java
+++ src/main/java/RaidersOnCall/items/consumables/SummonRaidScrollItem.java
@@ -1,27 +1,92 @@
 package RaidersOnCall.items.consumables;
 
+import necesse.engine.GameLog;
 import necesse.engine.localization.Localization;
-import necesse.engine.localization.message.GameMessage;
-import necesse.engine.localization.message.GameMessageBuilder;
 import necesse.engine.localization.message.LocalMessage;
-import necesse.engine.network.PacketReader;
+import necesse.engine.network.gameNetworkData.GNDItemMap;
 import necesse.engine.network.packet.PacketChatMessage;
+import necesse.engine.network.packet.PacketMobChat;
+import necesse.engine.network.server.ServerClient;
+import necesse.engine.registries.LevelEventRegistry;
+import necesse.engine.util.GameBlackboard;
+import necesse.engine.util.LevelIdentifier;
+import necesse.engine.world.worldData.SettlementsWorldData;
+import necesse.entity.levelEvent.settlementRaidEvent.HumanSettlementRaidLevelEvent;
+import necesse.entity.manager.EntityManager;
 import necesse.entity.mobs.PlayerMob;
 import necesse.gfx.gameTooltips.ListGameTooltips;
 import necesse.inventory.InventoryItem;
 import necesse.inventory.item.placeableItem.consumableItem.ConsumableItem;
+import necesse.level.maps.IncursionLevel;
 import necesse.level.maps.Level;
-import necesse.level.maps.levelData.settlementData.SettlementLevelData;
+import necesse.level.maps.levelData.settlementData.ServerSettlementData;
+
+import java.awt.geom.Line2D;
 
 public class SummonRaidScrollItem extends ConsumableItem {
     public SummonRaidScrollItem() {
         super(1, true);
+        this.itemCooldownTime.setBaseValue(2000);
+        this.dropsAsMatDeathPenalty = false;
+        this.rarity = Rarity.RARE;
     }
 
     @Override
-    public ListGameTooltips getTooltips(InventoryItem item, PlayerMob perspective) {
-        ListGameTooltips tooltips = super.getTooltips(item, perspective);
-        tooltips.add(new LocalMessage("tooltip", "summonraidtooltip1"));
+    public String canPlace(Level level, int x, int y, PlayerMob player, Line2D playerPositionLine, InventoryItem item, GNDItemMap mapContent) {
+        if (level.isClient())
+            return null;
+        if (level instanceof IncursionLevel)
+            return "inincursion";
+        if (!level.getIdentifier().equals(LevelIdentifier.SURFACE_IDENTIFIER))
+            return "notsurface";
+        if(!level.getWorldEntity().isNight())
+            return "notnight";
+        return null;
+    }
+
+    @Override
+    public InventoryItem onPlace(Level level, int x, int y, PlayerMob player, int seed, InventoryItem item, GNDItemMap mapContent) {
+        if (!level.isServer()) return item;
+
+        if (!level.getWorldEntity().isNight()) {
+            level.getServer().network.sendToAllClients(new PacketChatMessage("Raid can only be summoned at night."));
+            return item;
+        }
+
+        ServerClient serverClient = player.getServerClient();
+        ServerSettlementData settlementData = SettlementsWorldData.getSettlementsData(level).getServerDataAtTile(level.getIdentifier(), player.getTileX(), player.getTileY());
+        if (settlementData == null) {
+            player.getServerClient().sendChatMessage(new LocalMessage("raidersoncall", "nosettlement"));
+            return item;
+        }
+
+        if (!settlementData.spawnRaid()) {
+            String name = settlementData.networkData.getSettlementName().translate();
+            player.getServerClient().sendChatMessage(new LocalMessage("raidersoncall", "notenoughsettlers"));
+        }
+
+        level.getServer().network.sendToAllClients(new PacketChatMessage("Finally a worthy opponent! Our battle will be legendary!"));
+        item.setAmount(item.getAmount() - 1);
+        return item;
+    }
+
+    @Override
+    public InventoryItem onAttemptPlace(Level level, int x, int y, PlayerMob player, InventoryItem item, GNDItemMap mapContent, String error) {
+        if (level.isServer() && player != null && player.isServerClient() && error.equals("inincursion")) {
+            player.getServerClient().sendChatMessage(new LocalMessage("raidersoncall", "cannotsummoninincursion"));
+            return item;
+        }
+
+        if (level.isServer() && player != null && !error.equals("alreadyspawned"))
+            player.getServerClient().sendChatMessage(new LocalMessage("raidersoncall", error));
+
+        return item;
+    }
+
+    @Override
+    public ListGameTooltips getTooltips(InventoryItem item, PlayerMob perspective, GameBlackboard blackboard) {
+        ListGameTooltips tooltips = super.getTooltips(item, perspective, blackboard);
+        tooltips.add(Localization.translate("tooltip", "summonraidtooltip1"));
 
         LocalMessage line2 = new LocalMessage("tooltip", "summonraidtooltip2");
         StringBuilder builder = new StringBuilder();
@@ -31,26 +96,5 @@ public class SummonRaidScrollItem extends ConsumableItem {
         builder.append(line2.translate());
         tooltips.add(builder.toString());
         return tooltips;
-    }
-
-    @Override
-    public InventoryItem onPlace(Level level, int x, int y, PlayerMob player, InventoryItem item, PacketReader contentReader) {
-        if (!level.isServerLevel()) return item;
-
-        if (!level.getWorldEntity().isNight()) {
-            level.getServer().network.sendToAllClients(new PacketChatMessage("Raid can only be summoned at night."));
-            return item;
-        }
-
-        try{
-            SettlementLevelData levelData = SettlementLevelData.getSettlementData(level);
-            level.getServer().network.sendToAllClients(new PacketChatMessage("Finally a worthy opponent! Our battle will be legendary!"));
-            levelData.spawnRaid();
-            item.setAmount(item.getAmount() - 1);
-        } catch (NullPointerException ex){
-            level.getServer().network.sendToAllClients(new PacketChatMessage("Unable to spawn raid!"));
-            level.getServer().network.sendToAllClients(new PacketChatMessage("Either settlement is not found or number of settlers is lesser than 3."));
-        }
-        return item;
     }
 }
